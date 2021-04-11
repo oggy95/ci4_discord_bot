9f35971097ef77b7ce3a01243547ef6eb6810b50
horobchuk_o@itstep.academy <horobchuk_o@itstep.academy>
2021-04-11 12:08:15 +0300

    refactor, create loop for node war

--- bot_3.py
+++ bot_3.py
@@ -1,13 +1,10 @@
-import asyncio
 import logging
 import random
 import json
 
-import pytz
-import discord
 from aiohttp import ClientSession
 from discord.ext import commands
-
+from discord.ext.tasks import loop
 
 with open("config.json") as file:
     config = json.load(file)
@@ -15,9 +12,23 @@ with open("config.json") as file:
     TOKEN = config["api_token"]
     CHANNEL_ID = config["main_channel"]
     CYTATY_CHANNEL = config["channel_quotes"]
+    GAME_ALERT_CHANNEL = config["game_alert"]
 
+API_LINK = "https://meme-api.herokuapp.com/gimme/wholesomememes"
 
 client = commands.Bot(command_prefix='!')
+
+NODE_WAR_TEXT_ENGLISH = '''@⚔️Ci4⚔️ Hi everyone :slight_smile:
+Today we go T2 node at 19:00 by server time! Please be ready!
+If you can't be at NW tomorrow, write about it in #⛺day-offs
+Gather at voice channel in discord at 18:40 to discuss strategy on NW!
+Guild bosses will be after NW!'''
+
+NODE_WAR_TEXT_UKRAINIAN = '''@⚔️Ci4⚔️ Всім привіт 
+Сьогодні ми йдемо на вузол 2 рівня на 19:00 по серверному часу (21:00 по нашому). Тому просимо вашої активності!
+Якщо у вас не виходить бути на вузлі, відпишіть про це в #⛺day-offs
+Збирайтесь у голосовому чаті в 20:40 для обговорення тактики на вузлі!
+Гільд боси будуть сьогодні, відразу після вузла!'''
 
 
 @client.command(aliases=['випити', 'drink'], help='Прибухнем? | Cheers? :)')
@@ -37,7 +48,8 @@ async def command_node_war(ctx, arg):
     await ctx.send(f"@everyone у нас вузол о {arg}!!!\n@everyone В ДІСКОРДІ БУТИ ОБОВ'ЯЗКОВО!")
 
 
-@client.command(aliases=['х', 'храм', 'shrine', 's', 'Х', 'S'], help="['х', 'храм', 'shrine', 's'] Calling to the srine in great desert!")
+@client.command(aliases=['х', 'храм', 'shrine', 's', 'Х', 'S'], help="['х', 'храм', 'shrine', 's'] Calling to the "
+                                                                     "shrine in great desert!")
 async def command_shrine(ctx):
     await ctx.send("@everyone SHRINE|ХРАМ!!!")
 
@@ -47,7 +59,8 @@ async def command_death(ctx):
     await ctx.send("Смерть москалям!")
 
 
-@client.command(aliases=['чс', 'сонце', 'bs', 'sun', 'BS', 'ЧС'], help="['чс', 'сонце', 'bs', 'sun'] Calling to black sun")
+@client.command(aliases=['чс', 'сонце', 'bs', 'sun', 'BS', 'ЧС'], help="['чс', 'сонце', 'bs', 'sun'] Calling to black "
+                                                                       "sun")
 async def black_sun(ctx, arg):
     await ctx.send(f"@everyone Чорне Сонце сьогодні в {arg}!")
 
@@ -55,27 +68,34 @@ async def black_sun(ctx, arg):
 @client.command(aliases=['мем', 'meme'], help="['мем', 'memes'] Випадковий мем")
 async def memes(ctx):
     async with ClientSession() as cs:
-        async with cs.get("https://meme-api.herokuapp.com/gimme/wholesomememes") as r:
+        async with cs.get(API_LINK) as r:
             res = await r.json()  # returns dict
             await ctx.send(res['url'])
 
 
 @client.command(aliases=['р', 'l', 'Р', 'L'], help="['р', 'l'] Calling to Laytenn")
-async def laytenn(ctx, *args):
-   await ctx.send("@everyone РАЙТЕН! | LAYTENN!")
+async def laytenn(ctx):
+    await ctx.send("@everyone РАЙТЕН! | LAYTENN!")
 
 
 @client.command(name='рандом', help='Випадкова цитата гільдії')
-async def bosses(ctx, *args):
-    id = client.get_channel(CYTATY_CHANNEL)
-    messages = await id.history(limit=123).flatten()
+async def bosses(ctx):
+    channel_id = client.get_channel(CYTATY_CHANNEL)
+    messages = await channel_id.history(limit=123).flatten()
     await ctx.send(ctx.message.author.mention + ", що ти хочеш? От тобі цитата: " + random.choice(messages).content)
-           
+
 
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-         await ctx.send("шо, бля? | wtf!?")
+        await ctx.send("шо, бля? | wtf!?")
+
+
+@loop(seconds=10)
+async def colour_change():
+    channel_id = client.get_channel(GAME_ALERT_CHANNEL)
+    await channel_id.send(NODE_WAR_TEXT_ENGLISH)
+    await channel_id.send(NODE_WAR_TEXT_UKRAINIAN)
 
 
 logging.basicConfig(level=logging.INFO)
