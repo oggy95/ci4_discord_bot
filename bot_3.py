a85a6d630dd5e706aef5efb28adf40a4adec1e99
horobchuk_o@itstep.academy <horobchuk_o@itstep.academy>
2021-05-27 01:35:45 +0300

    change json config to yaml, refactor

--- bot_3.py
+++ bot_3.py
@@ -1,64 +1,29 @@
 import logging
 import random
-import json
+import yaml
 
 from aiohttp import ClientSession
 from discord.ext import commands
 from textblob import TextBlob
 from googletrans import Translator
 
-with open("config.json") as file:
-    config = json.load(file)
-    TELEGRAM_TOKEN = config["telegram_token"]
-    TOKEN = config["api_token"]
-    QUOTES_CHANNEL = config["channel_quotes"]
-    ENGLISH_CHANNEL = config["english_channel"]
-    RESTRICTED_CHANNELS = config["restricted_channels"]
-
-API_LINK = "https://meme-api.herokuapp.com/gimme/wholesomememes"
+with open("config.yaml") as file:
+    config = yaml.full_load(file)
 
 client = commands.Bot(command_prefix='!')
-
-LANGUAGE_CHOICE = {
-    "en": {
-        "uk": ":flag_ua:",
-        "pt": ":flag_pt:",
-        "it": ":flag_it:"
-    },
-    "pt": {
-        "uk": ":flag_ua:",
-        "en": ":flag_us:",
-        "it": ":flag_it:"
-    },
-    "uk": {
-        "en": ":flag_us:",
-        "pt": ":flag_pt:",
-        "it": ":flag_it:"
-    },
-    "ru": {
-        "en": ":flag_us:",
-        "pt": ":flag_pt:",
-        "it": ":flag_it:"
-    },
-    "it": {
-        "uk": ":flag_ua:",
-        "en": ":flag_us:",
-        "pt": ":flag_pt:"
-    }
-}
 
 
 @client.command(aliases=['мем', 'meme'], help="['мем', 'memes'] Випадковий мем")
 async def memes(ctx):
     async with ClientSession() as cs:
-        async with cs.get(API_LINK) as r:
+        async with cs.get(config["api_link"]) as r:
             res = await r.json()  # returns dict
             await ctx.send(res['url'])
 
 
 @client.command(name='рандом', help='Випадкова цитата гільдії')
 async def random_quote(ctx):
-    channel_id = client.get_channel(QUOTES_CHANNEL)
+    channel_id = client.get_channel(config["channel_quotes"])
     messages = await channel_id.history(limit=123).flatten()
     await ctx.send(ctx.message.author.mention + ", що ти хочеш? От тобі цитата: " + random.choice(messages).content)
 
@@ -71,20 +36,20 @@ async def on_command_error(ctx, error):
 
 @client.event
 async def on_message(message):
-    if message.channel.id in RESTRICTED_CHANNELS:
+    if message.channel.id in config["restricted_channels"]:
         if len(message.content) >= 3 \
                 and not message.content.startswith(("!", "http")) \
                 and message.author != client.user:
             language = TextBlob(message.content).detect_language()
             message_text = ""
             response_message = ""
-            if message.channel.id != int(ENGLISH_CHANNEL):
-                if language in LANGUAGE_CHOICE.keys():
-                    for key, value in LANGUAGE_CHOICE[language].items():
+            if message.channel.id != int(config["english_channel"]):
+                if language in config["language_choice"].keys():
+                    for key, value in config["language_choice"][language].items():
                         message_text = message_text + f"{value}{translator.translate(message.content, src=language, dest=key).text}\n"
                 response_message = f"{message.author.nick} - sorry, I don't understand you" if message_text == "" else \
                     f'{message.author.nick} said:\n>>> {message_text} '
-            elif message.channel.id == int(ENGLISH_CHANNEL) and language != "en":
+            elif message.channel.id == int(config["english_channel"]) and language != "en":
                 message_text = f":flag_us: {translator.translate(message.content, src=language, dest='en').text}\n"
                 response_message = f'{message.author.nick} said not in English. English text:\n>>> {message_text}'
             if response_message != "":
@@ -94,4 +59,4 @@ async def on_message(message):
 
 translator = Translator()
 logging.basicConfig(level=logging.INFO)
-client.run(TOKEN)
+client.run(config["api_token"])
