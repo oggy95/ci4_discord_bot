d71ada4b296685ddcfcb12c53ef3c52ceb9d0200
oggy95 <gorobchuk95@gmail.com>
2021-06-11 17:11:09 +0300

    fix None in response message of translating

--- bot_3.py
+++ bot_3.py
@@ -6,7 +6,7 @@ import json
 
 from aiohttp import ClientSession
 from discord.ext import commands
-from textblob import TextBlob
+from textblob import TextBlob, exceptions
 
 client = commands.Bot(command_prefix='!')
 
@@ -45,6 +45,7 @@ async def on_message(message):
         if len(message.content) >= 3 \
                 and not message.content.startswith(("!", "http")) \
                 and message.author != client.user:
+            nick = message.author.nick if message.author.nick is not None else str(message.author).split("#")[0]
             blob = TextBlob(message.content)
             language = blob.detect_language()
             message_text = ""
@@ -53,11 +54,11 @@ async def on_message(message):
                 if language in config["language_choice"].keys():
                     for key, value in config["language_choice"][language].items():
                         message_text = message_text + f"{value}{blob.translate(from_lang=language, to=key)}\n"
-                response_message = f"{message.author.nick} - sorry, I don't understand you" if message_text == "" else \
-                    f'{message.author.nick} said:\n>>> {message_text} '
+                response_message = f"{nick} - sorry, I don't understand you" if message_text == "" else \
+                    f'{nick} said:\n>>> {message_text} '
             elif message.channel.id == int(config["english_channel"]) and language != "en":
                 message_text = f":flag_us: {blob.translate(from_lang=language, to='en')}\n"
-                response_message = f'{message.author.nick} said not in English. English text:\n>>> {message_text}'
+                response_message = f'{nick} said not in English. English text:\n>>> {message_text}'
             if response_message != "":
                 await message.channel.send(response_message)
     await client.process_commands(message)
@@ -66,6 +67,7 @@ async def on_message(message):
 @client.event
 async def on_reaction_add(reaction, user):
     message = reaction.message
+    nick = message.author.nick if message.author.nick is not None else str(message.author).split("#")[0]
     if reaction.count == 1:
         blob = TextBlob(message.content)
         lg = blob.detect_language()
@@ -86,7 +88,7 @@ async def on_reaction_add(reaction, user):
             elif country_name["name"]["common"] == "Portugal":
                 language = "pr"
             elif country_name["name"]["common"] == "Ukraine":
-                language = "ua"
+                language = "uk"
             elif country_name["name"]["common"] == "Turkey":
                 language = "tr"
             elif country_name["name"]["common"] == "Poland":
@@ -94,9 +96,13 @@ async def on_reaction_add(reaction, user):
             else:
                 language = None
         if language != "":
-            translated_text = blob.translate(from_lang=lg, to=language)
-            message_text = f"{message.author.nick} said:\n>>> {reaction.emoji} {translated_text}\n"
-            await reaction.message.channel.send(message_text)
+            try:
+                translated_text = blob.translate(from_lang=lg, to=language)
+                message_text = f"{nick} said:\n>>> {reaction.emoji} {translated_text}\n"
+            except exceptions.NotTranslated:
+                message_text = f"{nick}: I'm sorry, cannot translate. Maybe languages is identical?"
+            finally:
+                await reaction.message.channel.send(message_text)
     await client.process_commands(message)
 
 
