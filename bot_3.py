39dff82095a1aa376681df3702a9109384b1d033
oggy95 <gorobchuk95@gmail.com>
2021-08-26 20:23:52 +0300

    voice support

--- bot_3.py
+++ bot_3.py
@@ -8,10 +8,75 @@ from discord import Embed
 from discord.ext import commands
 from aiohttp import ClientSession
 from textblob import TextBlob, exceptions
+from discord import FFmpegPCMAudio
+from discord.utils import get
+from youtube_dl import YoutubeDL
 
 client = commands.Bot(command_prefix='!')
 
 TIMEOUT = 30
+
+
+@client.command()
+async def play(ctx, url):
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    print(client)
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if not voice.is_playing():
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['url']
+        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        voice.is_playing()
+        await ctx.send('Bot is playing')
+
+    # check if the bot is already playing
+    else:
+        await ctx.send("Bot is already playing")
+        return
+
+
+@client.command()
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        await channel.connect()
+
+
+# command to resume voice if it is paused
+@client.command()
+async def resume(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if not voice.is_playing():
+        voice.resume()
+        await ctx.send('Bot is resuming')
+
+
+# command to pause voice if it is playing
+@client.command()
+async def pause(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice.is_playing():
+        voice.pause()
+        await ctx.send('Bot has been paused')
+
+
+# command to stop voice
+@client.command()
+async def stop(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice.is_playing():
+        voice.stop()
+        await ctx.send('Stopping...')
 
 
 @client.command(aliases=['мем', 'meme'], help="['мем', 'memes'] Випадковий мем")
@@ -55,7 +120,7 @@ async def nsfw(ctx):
 
 @client.event
 async def on_message(message):
-    if message.channel.id in config["restricted_channels"]\
+    if message.channel.id in config["restricted_channels"] \
             and message.author != client.user and message.content.startswith("!") is False:
         nick = message.author.nick if message.author.nick is not None else str(message.author).split("#", maxsplit=1)[0]
         print(f"{nick} said: ", message.content)
